62f65151089eedfe51299e5581a8cf60798e7f8c
Michael Diodoro <michael@Michaels-MacBook-Pro.local>
2017-05-02 17:48:54 -0700

    Fix index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.use(express.static(__dirname + '/client'));
 
 console.log(__dirname + '/client');
 
-app.listen(process.env.port || 3000, () => {
-  console.log('Server running on port: ' + 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server running on port: ' + process.env.PORT || 3000);
 });
