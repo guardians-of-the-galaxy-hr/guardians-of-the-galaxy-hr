933f313ff2c2a2f59906edbe7e63799c3a8d2e2a
Shawn Feng <shawnsfeng@gmail.com>
2017-05-09 20:30:50 -0700

    clean up motion detect file

--- client/components/motionDetect.js
+++ client/components/motionDetect.js
@@ -1,7 +1,6 @@
 angular.module('in-your-face')
 .controller('motionDetectCtrl', function() {
 
-
   var video = document.getElementById('video');
   var canvas = document.getElementById('canvas');
   var context = canvas.getContext('2d');
@@ -16,36 +15,11 @@ angular.module('in-your-face')
       context.strokeStyle = '#a64ceb';
       context.strokeRect(rect.x / 2.3 - 10, rect.y / 2.3 - 10, rect.width / 2.3, rect.height / 2.3); //rect.x, rect.y
       context.font = '8px Helvetica';
-      // context.font = '11px Helvetica';
-      context.fillStyle = "#fff";
-      // context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-      // context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+      context.fillStyle = '#fff';
       context.fillText('x: ' + rect.x + 'px', 0 + rect.width / 2 + 5, 0 + 11);
       context.fillText('y: ' + rect.y + 'px', 0 + rect.width / 2 + 5, 0 + 22);
     });
   });
-  // var gui = new dat.GUI();
-  // gui.add(tracker, 'edgesDensity', 0.1, 0.5).step(0.01);
-  // gui.add(tracker, 'initialScale', 1.0, 10.0).step(0.1);
-  // gui.add(tracker, 'stepSize', 1, 5).step(0.1);
-
-
-
-  // var objects = new tracking.ObjectTracker['face', 'eye', 'mouth'];
-
-  // objects.on('track', function(event) {
-  //   if (event.data.length === 0) {
-  //     // No objects were detected in this frame.
-  //   } else {
-  //     event.data.forEach(function(rect) {
-  //       // rect.x, rect.y, rect.height, rect.width
-  //     });
-  //   }
-  // });
-
-  // tracking.track('#myVideo', objects);
-
-
 });
 // .directive('motionDetect', function() {
 //   return {
@@ -59,3 +33,7 @@ angular.module('in-your-face')
 //     templateUrl: '../templates/motionDetect.html'
 //   };
 // });
+
+// context.font = '11px Helvetica';
+// context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+// context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
