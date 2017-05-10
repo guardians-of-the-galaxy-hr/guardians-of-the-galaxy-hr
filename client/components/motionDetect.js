4f3840ab8e781faf98d83e7db2c0ad6e868052e1
Shawn Feng <shawnsfeng@gmail.com>
2017-05-10 16:17:52 -0700

    implement take snapshot feature

--- client/components/motionDetect.js
+++ client/components/motionDetect.js
@@ -5,7 +5,7 @@ angular.module('in-your-face')
 
   // instantiate video camera and canvases
   var video = document.getElementById('video');
-  var canvas = document.getElementById('canvas');
+  var canvas = document.getElementById('photo-booth-canvas');
   var context = canvas.getContext('2d');
 
   // constants for picture links
@@ -18,26 +18,35 @@ angular.module('in-your-face')
   tracker.setEdgesDensity(0.1);
   tracking.track('#video', tracker, { camera: true });
 
-
   // change picture upon button clicking
   this.onChangePicButtonClicked = (personName) => {
-    if (personName === '') {
-      this.showRemoveEffectButton = false;
-    } else {
-      this.showRemoveEffectButton = true;
-    }
+    personName === '' ? this.showRemoveEffectButton = false : this.showRemoveEffectButton = true;
     selectedPicLink = personName;
   };
+
+  this.onTakeSnapshotButtonClicked = () => {
+    var hiddenCanvas = document.getElementById('photo-display-canvas');
+    hiddenCanvas.width = canvas.width;
+    hiddenCanvas.height = canvas.height;
+    var ctx = hiddenCanvas.getContext('2d');
+
+    // get video frame
+    ctx.drawImage(video, 0, 0, hiddenCanvas.width, hiddenCanvas.height);
+    // get canvas data
+    ctx.drawImage(canvas, 0, 0, hiddenCanvas.width, hiddenCanvas.height);
+    // draw both of them on the same hidden canvas
+    var imageData = ctx.getImageData(0, 0, 10, 10);
+    // var dataURI = hiddenCanvas.toDataURL('image/jpeg');
+    ctx.putImageData(imageData, 0, 0);
+  };
+
 
   // face detection event handling function
   tracker.on('track', function(event) {
     context.clearRect(0, 0, canvas.width, canvas.height);
     event.data.forEach(function(rect) {
-      console.log(rect);
-
       var imageObj = new Image();
       imageObj.src = selectedPicLink;
-
       if (rect.width >= 100 && rect.width < 205) {
         context.drawImage(imageObj, rect.x / 2.5, rect.y / 2.5 * 0.7, rect.width * 0.52, rect.width * 0.52 * 1.15);
       } else if (rect.width >= 205 && rect.width < 305) {
