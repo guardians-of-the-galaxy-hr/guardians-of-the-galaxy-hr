7b3eaf42409e9e39506bda47216d8f8dfc23a481
Shawn Feng <shawnsfeng@gmail.com>
2017-05-10 09:08:33 -0700

    add fred face to motion detect

--- client/components/motionDetect.js
+++ client/components/motionDetect.js
@@ -11,16 +11,33 @@ angular.module('in-your-face')
   tracking.track('#video', tracker, { camera: true });
   tracker.on('track', function(event) {
     context.clearRect(0, 0, canvas.width, canvas.height);
+    var imageObj = new Image();
+    imageObj.src = './assets/fred_pic.gif';
+    imageObj.width = 40;
+    imageObj.height = 50;
     event.data.forEach(function(rect) {
-      context.strokeStyle = '#a64ceb';
-      context.strokeRect(rect.x / 2.3 - 10, rect.y / 2.3 - 10, rect.width / 2.3, rect.height / 2.3); //rect.x, rect.y
-      context.font = '8px Helvetica';
-      context.fillStyle = '#fff';
-      context.fillText('x: ' + rect.x + 'px', 0 + rect.width / 2 + 5, 0 + 11);
-      context.fillText('y: ' + rect.y + 'px', 0 + rect.width / 2 + 5, 0 + 22);
+      context.drawImage(imageObj, rect.x / 2.3 - 15, rect.y / 2.3 - 40, 90, 105);
+      // context.strokeStyle = '#a64ceb';
+      // context.strokeRect(rect.x / 2.3 - 10, rect.y / 2.3 - 10, rect.width / 2.3, rect.height / 2.3); //rect.x, rect.y
+      // context.font = '8px Helvetica';
+      // context.fillStyle = '#fff';
+      // context.fillText('x: ' + rect.x + 'px', 0 + rect.width / 2 + 5, 0 + 11);
+      // context.fillText('y: ' + rect.y + 'px', 0 + rect.width / 2 + 5, 0 + 22);
     });
   });
 });
+
+
+// var imageObj = new Image();
+
+// imageObj.onload = function() {
+//   context.drawImage(imageObj, 69, 50);
+// };
+// imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg'
+
+
+
+
 // .directive('motionDetect', function() {
 //   return {
 //     scope: {
