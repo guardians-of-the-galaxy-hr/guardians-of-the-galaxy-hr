0a8a716608b7c64d845e4f2484fd628990a71428
Mahima Srikanta <mahima.srikanta@gmail.com>
2017-05-13 14:14:16 -0700

    callback url protocol

--- config/facebook.js
+++ config/facebook.js
@@ -3,6 +3,7 @@ var facebookAuth = {
   clientID: '282508915543842',
   clientSecret: '4741930c1f55e715610de159d669900a',
   callbackURL: '/auth/facebook/callback',
+  protocol: process.env.PROTOCOL,
   passReqToCallback: true,
   scope: ['user_friends'],
   'profileFields': ['id', 'emails', 'name', 'friends']
