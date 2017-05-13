579c3a696f68871a0b9d6f5aacc66861fdb885b9
Mahima Srikanta <mahima.srikanta@gmail.com>
2017-05-13 13:18:58 -0700

    Facebook Auth Implemented

---
+++ config/facebook.js
@@ -0,0 +1,13 @@
+var facebookAuth = {
+
+  clientID: '282508915543842',
+  clientSecret: '4741930c1f55e715610de159d669900a',
+  callbackURL: '/auth/facebook/callback',
+  passReqToCallback: true,
+  scope: ['user_friends'],
+  'profileFields': ['id', 'emails', 'name', 'friends']
+
+
+};
+module.exports = facebookAuth;
+
